8e9c423c694f6fa77d183f729a0ff5235da6c079
Akhilesh G <akhilesh.gollapudi@outlook.com>
2026-02-28 12:00:22 -0700

    Changed Module Format

--- tests/test_terminalExecute.py
+++
@@ -1,8 +0,0 @@
-import terminalExecute
-
-def test_import():
-    # This just ensures the package is installed and importable
-    assert terminalExecute.__name__ == "terminalExecute"
-
-def test_placeholder():
-    assert True
